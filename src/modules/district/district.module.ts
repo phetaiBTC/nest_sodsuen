bdfd4f43967ece1401c299654e6582094a69b581
unknown <phetaibtc@gmail.com>
2025-06-25 20:35:13 +0700

    pp

--- src/modules/district/district.module.ts
+++ src/modules/district/district.module.ts
@@ -3,9 +3,10 @@ import { DistrictService } from './district.service';
 import { DistrictController } from './district.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { District } from './entities/district.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([District])],
+  imports: [TypeOrmModule.forFeature([District,User])],
   controllers: [DistrictController],
   providers: [DistrictService],
   exports: [DistrictService],
